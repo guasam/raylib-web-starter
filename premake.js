849dc5aff199cf5f9acea54ab91a75164b237160
guasam <guasam@codesbiome.com>
2021-09-15 03:23:21 +0530

    Add shell.html and modify scripts usage

--- premake.js
+++ premake.js
@@ -1,41 +1,45 @@
-var fs = require('fs');
-var obj_dir = './obj';
-var build_dir = './build';
-var prepareDeploy = process.argv.includes('prepare-deploy');
+const fs = require('fs');
+const path = require('path');
 
+let obj_dir = path.resolve(__dirname, 'obj');
+let build_dir = path.resolve(__dirname, 'build');
+let prepareDeploy = process.argv.includes('prepare-deploy');
+
+//-----------------------------------------
+// Create missing directories for make
+//-----------------------------------------
+
+if (!fs.existsSync(obj_dir)) {
+  fs.mkdirSync(obj_dir);
+  console.log('Created directory: ' + obj_dir);
+}
+
+if (!fs.existsSync(build_dir)) {
+  fs.mkdirSync(build_dir);
+  console.log('Created directory: ', build_dir);
+}
+
+console.log('premake: executed successfully!');
+
+//-----------------------------------------
+// Prepare deployment
+//-----------------------------------------
 if (prepareDeploy) {
-  //-----------------------------------------
-  // prepare deployment on ghpages
-  //-----------------------------------------
   const glob = require('glob');
-  const fs = require('fs');
-  var path = require('path');
 
   // Find existing html file to rename as 'index.html'
-  glob(__dirname + '/**/*.html', {}, (err, files) => {
-    if (files.length) {
-      const source = files[0];
-      const destination = path.join(path.dirname(source), 'index.html');
-      fs.copyFile(source, destination, (err) => {
-        if (err) throw err;
-        console.log(`(${path.basename(source)}) was copied as (index.html)`);
-      });
+  glob(build_dir + '/**/*.html', {}, (err, files) => {
+    if (!files.length) return;
+    // Check if first index contains index.html
+    if (path.basename(files[0]) === 'index.html') {
+      // Remove this file index
+      files.splice(0, 1);
     }
+
+    // Copy next file index to index.html
+    fs.copyFile(files[0], path.join(build_dir, 'index.html'), (err) => {
+      if (err) throw err;
+      console.info(`(${path.basename(files[0])}) was copied as (index.html)`);
+    });
   });
-} else {
-  //-----------------------------------------
-  // Create missing directories for make
-  //-----------------------------------------
-
-  if (!fs.existsSync(obj_dir)) {
-    fs.mkdirSync(obj_dir);
-    console.log('Created directory: ' + obj_dir);
-  }
-
-  if (!fs.existsSync(build_dir)) {
-    fs.mkdirSync(build_dir);
-    console.log('Created directory: ', build_dir);
-  }
-
-  console.log('premake: executed successfully!');
 }
