584818ca4b884835bc5b57237eade3af871e878b
guasam <guasam@codesbiome.com>
2021-09-15 16:17:47 +0530

    Force viewport canvas scaling, align texts

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
  **/
 
 #include "raylib.h"
+#include <stdio.h>
 
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -13,19 +14,53 @@
 
 void UpdateDrawFrame(void);
 Texture2D iconTexture;
-const int screenWidth = 800;
-const int screenHeight = 600;
+int screenWidth = 414;
+int screenHeight = 736;
 int rotation = 0;
+Vector2 textSize = { 0.0f, 0.0f };
+int fontSize = 20;
+int fontSpacing = 2; // For correct alignement?
+const char* headText = "Raylib Web Starter";
+const char* strictText = "Copyright (c) 2021 Guasam";
+
+
+#if defined(PLATFORM_WEB)
+EM_JS(int, get_canvas_width, (), {
+return available_screen_width();
+    }
+);
+
+EM_JS(int, get_canvas_height, (), {
+return available_screen_height();
+    }
+);
+#endif
+
+int get_canvas_width();
+int get_canvas_height();
 
 int main(void)
 {
+
+#if defined(PLATFORM_WEB)
+    int canvasWidth = get_canvas_width();
+    int canvasHeight = get_canvas_height();
+
+    screenWidth = canvasWidth;
+    screenHeight = canvasHeight;
+
+    printf("Checking %d %d", canvasWidth, canvasHeight);
+#endif
+
     InitWindow(screenWidth, screenHeight, "Raylib Web Starter");
 
     // Load texture (must be after initializing window to get OpenGL Context)
     iconTexture = LoadTexture("resources/icon.png");
 
 #if defined(PLATFORM_WEB)
+
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+
 #else
 
     SetTargetFPS(60);
@@ -49,28 +84,27 @@ void UpdateDrawFrame(void)
 
     ClearBackground(RAYWHITE);
 
-    DrawText("Raylib Web Starter", 190, 200, 20, LIGHTGRAY);
+    // Draw text 
+    textSize = MeasureTextEx(GetFontDefault(), headText, fontSize, fontSpacing);
+    DrawText(headText, (screenWidth - textSize.x) / 2.0f, textSize.y + 20, fontSize, GRAY);
+    //DrawRectangleLines((screenWidth - textSize.x) / 2.0f, textSize.y, textSize.x, textSize.y, RED);
+
+    // Draw text
+    textSize = MeasureTextEx(GetFontDefault(), strictText, fontSize, fontSpacing);
+    DrawText(strictText, (screenWidth - textSize.x) / 2.0f, screenHeight - textSize.y - 20, fontSize, LIGHTGRAY);
+    //DrawRectangleLines((screenWidth - textSize.x) / 2.0f, screenHeight - textSize.y - 20, textSize.x, textSize.y, RED);
 
     // Increment rotation
     rotation++;
 
-    // Draw icon texture
-   /* DrawTexturePro(
-        iconTexture,
-        Rectangle{ 0.0f, 0.0f, 1000, 1000 },
-        Rectangle{ screenWidth / 2, screenHeight / 2, iconTexture.width * 2.0f, iconTexture.height * 2.0f },
-        Vector2{ (float)iconTexture.width, (float)iconTexture.height },
-        (float)rotation,
-        WHITE
-    );*/
-
+    // Scale image
     float scale = 0.75f;
 
     DrawTexturePro(
         iconTexture,
         Rectangle{ 0, 0, (float)iconTexture.width, (float)iconTexture.height },
-        Rectangle{ screenWidth / 2, screenHeight / 2, (float)iconTexture.width * scale, (float)iconTexture.height * scale },
-        Vector2{ (float) iconTexture.width / 2 * scale, (float) iconTexture.height / 2 * scale },
+        Rectangle{ screenWidth / 2.0f, screenHeight / 2.0f, iconTexture.width * scale, iconTexture.height * scale },
+        Vector2{ iconTexture.width / 2.0f * scale, iconTexture.height / 2.0f * scale },
         (float)rotation,
         WHITE
     );
